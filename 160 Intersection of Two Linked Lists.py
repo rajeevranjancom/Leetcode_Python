ae11a207da0047655c70de18aeca55727b125afe
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-26 03:00:12 +0530

    Update 160 Intersection of Two Linked Lists.py

--- 160 Intersection of Two Linked Lists.py
+++ 160 Intersection of Two Linked Lists.py
@@ -19,8 +19,8 @@ If the two linked lists have no intersection at all, return null.
 The linked lists must retain their original structure after the function returns.
 You may assume there are no cycles anywhere in the entire linked structure.
 Your code should preferably run in O(n) time and use only O(1) memory.
+Author: Rajeev Ranjan
 """
-__author__ = 'Daniel'
 
 
 class ListNode:
