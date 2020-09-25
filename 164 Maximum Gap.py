3ca1f77954d7c24d833de869e7afb255d3d429f6
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-26 03:01:14 +0530

    Update 164 Maximum Gap.py

--- 164 Maximum Gap.py
+++ 164 Maximum Gap.py
@@ -6,8 +6,9 @@ Try to solve it in linear time/space.
 Return 0 if the array contains less than 2 elements.
 
 You may assume all elements in the array are non-negative integers and fit in the 32-bit signed integer range.
+Author: Rajeev Ranjan
 """
-__author__ = 'Daniel'
+
 import sys
 
 
