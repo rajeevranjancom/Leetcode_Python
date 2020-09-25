f05c23b340965491c7dd86ed7eb24a7fdf731ecb
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-09-26 02:56:44 +0530

    Update 152 Maximum Product Subarray.py

--- 152 Maximum Product Subarray.py
+++ 152 Maximum Product Subarray.py
@@ -3,8 +3,8 @@ Find the contiguous subarray within an array (containing at least one number) wh
 
 For example, given the array [2,3,-2,4],
 the contiguous subarray [2,3] has the largest product = 6
+Author: Rajeev Ranjan
 """
-__author__ = 'Danyang'
 
 
 class Solution(object):
