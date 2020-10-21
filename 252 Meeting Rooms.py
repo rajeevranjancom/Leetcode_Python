787d69ed6f59aeddcc4425ecde1b572c036439da
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-10-22 01:05:49 +0530

    252 Meeting Rooms.py

---
+++ 252 Meeting Rooms.py
@@ -0,0 +1,27 @@
+"""
+Premium Question
+Author: Rajeev Ranjan
+
+"""
+import operator
+
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+class Solution:
+    def canAttendMeetings(self, intervals):
+        """
+
+        :type intervals: list[Interval]
+        :rtype: bool
+        """
+        intervals.sort(key=operator.attrgetter("start"))
+        for i in xrange(len(intervals)-1):
+            if intervals[i].end > intervals[i+1].start:
+                return False
+
+        return True
+
