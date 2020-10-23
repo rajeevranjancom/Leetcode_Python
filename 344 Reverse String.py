a7356c329bbe142ada73106a652c4b92cb205bfb
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-10-23 23:37:31 +0530

    344 Reverse String.py

---
+++ 344 Reverse String.py
@@ -0,0 +1,14 @@
+"""
+Write a function that takes a string as input and returns the string reversed.
+Author: Rajeev Ranjan
+
+"""
+
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
