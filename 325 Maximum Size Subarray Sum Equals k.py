ee4b42fc197c7c025b649951ab95fca79ead2485
Rajeev Ranjan <50344183+rajeev-ranjan-au6@users.noreply.github.com>
2020-10-23 23:26:30 +0530

    325 Maximum Size Subarray Sum Equals k.py

---
+++ 325 Maximum Size Subarray Sum Equals k.py
@@ -0,0 +1,28 @@
+"""
+Premium Question
+Author: Rajeev Ranjan
+
+"""
+
+
+class Solution(object):
+    def maxSubArrayLen(self, A, k):
+        """
+        Search problem
+        :type A: List[int]
+        :type k: int
+        :rtype: int
+        """
+        m = {0: -1}  # initial condition, sum -> idx
+        maxa = 0
+        s = 0
+        for i in xrange(len(A)):
+            s += A[i]
+            t = s - k  # s - t = k
+            if t in m:
+                maxa = max(maxa, i - m[t])
+
+            if s not in m:
+                m[s] = i
+
+        return maxa
